cdbb164310218b6728561fc813cc0ae33a92c117
Dawid108 <gustowskidawid108@gmail.com>
2025-12-27 11:55:22 +0100

    Update 20260106_sw.js

--- 20260106_sw.js
+++ 20260106_sw.js
@@ -1,8 +1,32 @@
+const CACHE_NAME = "pwa-cache-v1";
+
 self.addEventListener("install", e => {
-  console.log("Service Worker: install");
   self.skipWaiting();
 });
 
 self.addEventListener("activate", e => {
-  console.log("Service Worker: activate");
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME)
+            .map(k => caches.delete(k))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(e.request, clone);
+        });
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
