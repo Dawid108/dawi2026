096c67dd00c31982953ebad41de40f4a19b130ff
Dawid108 <gustowskidawid108@gmail.com>
2026-06-10 22:02:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./20260106_sw.js');
 }
-
-// Dodawanie 1 punktu co 5 sekund
-setInterval(() => {
-    dawid108++;
-    localStorage.setItem("dawid108", dawid108);
-    console.log("Dodano punkt. Aktualnie:", dawid108);
-}, 5000);
